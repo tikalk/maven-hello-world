e890e2c022b39b898092c67bbe475cb5163b5bfc
Yoram Michaeli <yorammi@tikalk.com>
2017-10-02 22:54:47 +0300

    Star Wars sequence display

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,8 +6,25 @@ package com.mycompany.app;
  */
 public class App 
 {
-    public static void main( String nameToDisplay )
+    public static void main( String[] args )
     {
-        System.out.println( "Hello "+ nameToDisplay );
+        if (args.length != 1) 
+        {
+            try 
+            {
+                int starWarsSequence = Integer.parseInt(args[0]);
+                System.out.println("Star Wars sequance: " + starWarsSequence.toString());
+            }
+            catch (NumberFormatException e) 
+            {
+                System.err.println("Argument" + args[0] + " must be an integer.");
+                System.exit(2);
+            }
+        }
+        else
+        {
+            System.err.println("There must be exactly 1 argument.");
+            System.exit(1);
+        }
     }
 }
