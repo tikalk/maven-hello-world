8b8f637fe467e5e4004d0cd9f94fb6d6ae75fb7e
Yoram Michaeli <yorammi@tikalk.com>
2017-10-02 22:39:10 +0300

    Add argument - nameToDisplay

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,24 +6,8 @@ package com.mycompany.app;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String nameToDisplay )
     {
-        System.out.println( "Hello World!" );
-
-        System.out.println("A NullPointerException is a drag...");
-        Object myObject = null;
-        /**
-         * Checker prevents this from compiling...
-         */
-        // System.out.println("myObject: " + myObject.toString());
-        /**
-         * ... which is simply fantastic. It shows:
-         *
-         * error: [dereference.of.nullable] dereference of possibly-null
-         * reference myObject
-         *
-         * http://checkerframework.org
-         */
-        System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
+        System.out.println( "Hello "+ nameToDisplay );
     }
 }
